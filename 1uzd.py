8dd8b3859b56673f40d748f1957405059e0a90bd
paulabardule <paula.bardule@grobinasskola.lv>
2023-11-02 10:45:22 +0200

    Init commit

--- 1uzd.py
+++ 1uzd.py
@@ -8,9 +8,9 @@ cipari 1,2,3,4,5,6,7,8,9
     1984
 """
 
-class AAA:
-    def __init__(self):
-        self.rom_cip={
+class AAA: # klasi definē "class" nosaukums AAA, aiz nosaukuma vienmer :
+    def __init__(self): #klasei ir konstruktors (__init__) un metodes (funkcijas) iekavas tiek rakstiti parametri (klasesiekšējais mainīgais)
+        self.rom_cip={ # self.rom_cip definēts iekšējais mainīgais { norada ka ta ir datu struktura} ja tas ir tuksas tad ta ir tuksa vardnica ar vardnicu jasaprot tabulu (1 kolonna ir atslega otra vertiba)
             1: 'I',
             4:'IV',
             5: 'V',
@@ -26,15 +26,15 @@ class AAA:
             1000: 'M'
         }
     def uz_romu(self, num):
-       result=""
-       for value, sk in sorted(self.rom_cip.items(),key=lambda x:x[0], reverse=True):
+       result="" # result ir ieksejais mainigais "" - saraksts bet "tuksa simbolu virkne"
+       for value, sk in sorted(self.rom_cip.items(),key=lambda x:x[0], reverse=True): # value un sk ir cikla skaititaji in norada ka tie darbosies apganala sorted norada ka pagabals ir sakartots lambda 
            while num>=value:
                result+=sk
                num-=value
        return result
 
-skaitlis=2
-konvertet=AAA()
-rom_sks=konvertet.uz_romu(skaitlis)
+skaitlis=2 # var likt jebkuru skaitli kuru grib izdrukat
+konvertet=AAA()# define jaunu objektu jauns objekts ar nosaukumu konvertet objektus var izveidot tikai tad kad ir klase
+rom_sks=konvertet.uz_romu(skaitlis) # mainigais ir ar nosaukumu roma_sks mainigajam pieksir vertibu objektam tiek izsaukta metode ar . 
 print(f"{skaitlis} romiešu ciparos ir {rom_sks}.")
     
--- 2uzd.py
+++ 2uzd.py
@@ -2,4 +2,14 @@
 Iegūt ziņas, izmantojot rss no google.lv.
 
 """
+import feedparser # feedparser ir bibliotēka (instalet ar pip install feedparser)
 
+# noradu rss plusmas url, kas satur google.lv zinas
+rss_url="https://news.google.com/rss?hl=lv&gl=LV&ceid=LV:lv"
+# lejupieladet un izanalizet rss plusmu
+feed=feedparser.parse(rss_url)
+# japarada zinu virsrakstus un saites
+for entry in feed.entries:
+    print("Virsraksts", entry.title)
+    print("Saite", entry.link)
+    print("\n")
